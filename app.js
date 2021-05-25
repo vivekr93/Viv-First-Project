57bec4d40f02c6a1d9acb58ba76ad16d0812cb7b
Vivek Ramakrishnan <vivekr93@gmail.com>
2021-05-25 02:59:15 -0700

    restricted color values

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function randomRGB(){
-    const r = Math.floor(Math.random() * 256)
+    const r = Math.floor(Math.random() * 10)
     const g = Math.floor(Math.random() * 256)
     const b = Math.floor(Math.random() * 256)
     return `rgb(${r},${g},${b})`
